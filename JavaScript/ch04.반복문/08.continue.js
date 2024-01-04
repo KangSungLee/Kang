7ae6048127a5e36ff4094fad4012e6cbbcfbc731
kangsunglee <rkdtjd1020@naver.com>
2024-01-04 22:29:45 +0900

    집

--- JavaScript/ch04.반복문/08.continue.js
+++ JavaScript/ch04.반복문/08.continue.js
@@ -5,9 +5,17 @@ for (let i = 1; i <= 10; i += 2)
 console.log(sum);
 
 sum = 0;
-for (let i = 0; i <= 10; i++){
+for (let i = 0; i <= 10; i++) {
     if (i % 2 == 0)
         continue;
     sum += i;
 }
 console.log(sum);
+
+// 짝수
+sum = 0;
+for (let i = 2; i <= 10; i += 2){
+    console.log(sum)
+    sum += i;
+}
+console.log(sum)
