a90aac72d05720562bb97b3414835bc01dff9a66
KangSungLee <rkdtjd1020@naver.com>
2024-01-09 09:43:24 +0900

    연습문제

--- JavaScript/ch07.표준내장객체/04.String연습.js
+++ JavaScript/ch07.표준내장객체/04.String연습.js
@@ -36,7 +36,7 @@ console.log(countArr); */
 
 // 2. 디지털 시계, 00:00 ~ 23:59
 // 하루동안 3이 표시되는 시간은 몇초인가?
-let sum = 0;
+/* let sum = 0;
 for (let h = 0; h <= 24; h++) {
     if (h % 10 === 3)
         sum += 60 * 60
@@ -49,7 +49,19 @@ for (let h = 0; h <= 24; h++) {
                     if (n % 10 === 3)
                         sum += 1
 }
-console.log(sum)
+console.log(sum) */
+
+/* let totalSec = 0;    
+for (let h = 0; h < 24; h++) {
+    for (let m = 0; m < 60; m++) {
+        if (h % 10 == 3 || Math.floor(m / 10) == 3 || m % 10 == 3) { 
+
+            totalSec += 60;
+        }
+    }
+}
+console.log("total seconds: " + totalSec);
+ */
 
 // 3. 두개의 세자리수를 곱해서 나온 결과가 palindrome일때
 // 가장 큰 palinderome 수와 어떤 수를 곱해서 나온 결과인가?
@@ -64,11 +76,39 @@ function isPalinderome(str){
     return str = reverseStr;
 }
 
-console.log(isPalinderome(str));
+console.log(isPalinderome(str)); */
+
+function isPalindrome(str) {
+    str = String(str);
+    let reverseStr = str.split('').reverse().join('');
+    return str == reverseStr;
+}
+
+let all = [];
+let max = 0;
+let num1 = 100, num2 = 100;
+for (let i = 100; i <= 999; i++) {
+    for (let k = 100; k <= 999; k++) {
+        let palindrome = i * k;
+        if (isPalindrome(palindrome) == true)
+            all.push(Number(palindrome));
+    }
+    for (let num of all) {
+        if (num > max)
+            max = num;
+    }
+    if (max % i == 0) {
+        num1 = i;
+        num2 = max / i;
+    }
+}
+console.log(`가장 큰 Palindrome는 ${max}이고, 이는 ${num1} X ${num2}입니다.`);
 
 
- */
 
 
 //4. C:/Workspace/Webproject/03.JavaScript/ch07.표준내장객체/04.String연습.js
 //에서 파일명 (04.String연습.js)만 출력하세요.
+
+let file = 'C:/Workspace/Webproject/03.JavaScript/ch07.표준내장객체/04.String연습.js'
+console.log(file.substring(50, 64))
